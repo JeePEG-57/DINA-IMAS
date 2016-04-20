872222ce52780294ed12412411818e000c756a25
medveds <medveds@hpc-login4.iter.org>
2016-04-20 16:26:18 +0200

    Clean machines/iter and use tokamak data files extracted from ITER.mat. Matlab scripts to extract/clean to/up $KEPLER are in imas/co-simulation

---
+++ imas/co-simulation/CleanTokamakData.m
@@ -0,0 +1,34 @@
+% The script writes tokamak data files from one *.mat file.  
+
+%clear all
+
+%FilesDir = '~lukashv/kepler';
+cmd=sprintf('/bin/bash --login -c ''echo "$KEPLER"''');
+[r,s] = system(cmd);  s(s==10)=[]; s(s==13)=[];
+FilesDir = s;
+
+
+%% Loading *.mat
+load('ITER.mat');
+
+
+%% Cleaning files
+
+%mkdir([FilesDir]);
+
+for i = 1:length(ITER.files)
+    
+    %Write_any(ITER.files(i).name, FilesDir, ITER.files(i).data);
+    delete([FilesDir filesep() ITER.files(i).name]);
+       
+end
+
+%mkdir([FilesDir filesep() 'imp']);
+
+for i = 1:length(ITER.imp.files)
+    
+    %Write_any(ITER.imp.files(i).name, [FilesDir filesep() 'imp'], ITER.imp.files(i).data);
+    delete([FilesDir filesep() 'imp' filesep() ITER.imp.files(i).name]);   
+end
+
+rmdir([FilesDir filesep() 'imp']);
---
+++ imas/co-simulation/WriteTokamakData.m
@@ -0,0 +1,31 @@
+% The script writes tokamak data files from one *.mat file.  
+
+%clear all
+
+%FilesDir = '~lukashv/kepler';
+cmd=sprintf('/bin/bash --login -c ''echo "$KEPLER"''');
+[r,s] = system(cmd); s(s==10)=[]; s(s==13)=[];
+FilesDir = s;
+
+
+%% Loading *.mat
+load('ITER.mat');
+
+
+%% Writing to files
+
+%mkdir([FilesDir]);
+
+for i = 1:length(ITER.files)
+    
+    Write_any(ITER.files(i).name, FilesDir, ITER.files(i).data);
+       
+end
+
+mkdir([FilesDir filesep() 'imp']);
+
+for i = 1:length(ITER.imp.files)
+    
+    Write_any(ITER.imp.files(i).name, [FilesDir filesep() 'imp'], ITER.imp.files(i).data);
+       
+end
