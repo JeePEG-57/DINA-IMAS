53e69c493aa11e46799f7d5d4810ffce4dc8536c
Max Dubrov <comrademax@yandex.ru>
2016-11-07 11:00:54 +0100

    Defaults in WriteTokamakDataF.m are changed.

--- imas/co-simulation/WriteTokamakDataF.m
+++ imas/co-simulation/WriteTokamakDataF.m
@@ -1,20 +1,28 @@
 % The script writes tokamak data files from one *.mat file.  
-function MatFile = WriteTokamakDataF(FilesDir,MatFile)
+function [MatFile] = WriteTokamakDataF(varargin)
 %clear all
-if nargin<2,
-    MatFile = 'ITER.mat';
+if length(varargin) < 2
+    [MyPath,~,~] = fileparts(mfilename('fullpath'));
+    MatFile = [MyPath '/../../machines/iter/ITER_7.5MA/' 'ITER.mat'];
+%     MatFile = 'ITER.mat';
+else
+    MatFile = varargin{2};
 end
-if nargin<1 || isempty(FilesDir),
+if length(varargin) < 1
     %FilesDir = '~lukashv/kepler';
     %cmd=sprintf('/bin/bash --login -c ''echo "$KEPLER"''');
     %cmd = 'echo $KEPLER'; [r,s] = system(cmd); s(s==10)=[]; s(s==13)=[];  
     FilesDir = getenv('KEPLER');
+    if isempty(FilesDir)
+        FilesDir = [getenv('HOME') '/kepler'];
+    end
+else
+    FilesDir = varargin{1};
 end
 
 
 %% Loading *.mat
 load(MatFile);
-
 
 %% Writing to files
 
