6210919076ebce5bfd7954c0605b3aed58081536
medveds <medveds@hpc-login02.iter.org>
2020-06-11 17:14:36 +0200

    EqTest: python check_EqTest.py in right place

--- imas/ci_scripts/ci_test.sh
+++ imas/ci_scripts/ci_test.sh
@@ -106,6 +106,7 @@ elif [ $input == 'EqTest' ]; then
   kepler -runwf -nogui $PWD/EqTest.xml | tee EqTest.log
   
   # check psi accuracy
+  cd ../EqTestRegression
   python check_EqTest.py | tee -a EqTest.log 
 
 #   # ---> Extract executable from the artifact and run the wrapper
