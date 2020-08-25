afa5c0c33eeb456cde065c0db2eedf56007becdc
Max Dubrov <comrademax@yandex.ru>
2020-08-25 15:08:48 +0200

    Empty actors are added to separate density control for pellets and valves See kepler workflow Dina_Scenario_ScheduleSDN_DensityControl.xml

---
+++ imas/astra_transp/density_control_valve.f90
@@ -0,0 +1,44 @@
+subroutine density_control_valve(summary_in, ctrl_out)
+
+use ids_schemas
+use ids_routines
+implicit none
+
+
+type (ids_summary) :: summary_in
+real (ids_real) :: ctrl_out(*)
+
+integer :: i,npo
+
+! used npo as kint in transp2
+parameter (npo=200)
+
+!integer, parameter :: DP = kind(1.0d0)
+
+real(ids_real) :: Ne, Ni
+
+
+real(ids_real) :: tt
+
+	character *20 apr
+
+
+tt = summary_in%time(1)
+
+Ne = summary_in%volume_average%n_e%value(1)
+Ni = summary_in%volume_average%n_i_total%value(1)
+
+print *,'  Density controller tt Ne Ni',tt,Ne,Ni
+
+
+   71 FORMAT(20X,A20/,(6(1pE10.3)))
+
+
+
+    
+ctrl_out(1) = 1.d0
+ctrl_out(2) = 1.d0    
+    
+    
+return
+end subroutine
