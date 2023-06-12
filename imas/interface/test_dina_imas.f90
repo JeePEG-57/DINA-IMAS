6c1acc4cf91bac700b382d9f5625e2ea5f341ac5
Max Dubrov <comrademax@yandex.ru>
2023-06-12 10:30:03 +0200

    Separate input database entry to look for pulse_schedule

--- imas/interface/test_dina_imas.f90
+++ imas/interface/test_dina_imas.f90
@@ -82,9 +82,11 @@ real (ids_real) :: arr_in1(501), arr_out1(501)
 character (len=255) :: user_default
 character (len=255) :: user_out, database_out
 character (len=255) :: user_prs, database_prs
+character (len=255) :: user_psch, database_psch
 character (len=255) :: user_transp='', database_transp=''
 integer :: pulse_prs=-1, run_prs=-1
 integer :: pulse_out=-1, run_out=-1
+integer :: pulse_psch=-1, run_psch=-1
 integer :: pulse_transp=-1, run_transp=-1
 
 ! Workflow parameters
@@ -144,6 +146,11 @@ print *,' Using workflow config file: ', ConfigFile
 call file2buffer(ConfigFile, io_unit, buffer)
 call xml2eg_parse_memory(buffer, doc)
 
+  call xml2eg_get(doc, 'pulse_schedule/user', user_psch)
+  call xml2eg_get(doc, 'pulse_schedule/database', database_psch)
+  call xml2eg_get(doc, 'pulse_schedule/pulse', pulse_psch)
+  call xml2eg_get(doc, 'pulse_schedule/run', run_psch)
+  
   call xml2eg_get(doc, 'input_start/user', user_prs)
   call xml2eg_get(doc, 'input_start/database', database_prs)
   call xml2eg_get(doc, 'input_start/pulse', pulse_prs)
@@ -172,6 +179,7 @@ deallocate(buffer)
 
 
 if (trim(user_prs).eq.'') user_prs = user_default
+if (trim(user_psch).eq.'') user_psch = user_default
 if (trim(user_transp).eq.'') user_transp = user_default
 user_out = user_default
 
@@ -240,13 +248,18 @@ endif
 
 call ids_get(idx0,"wall",wall)
 call ids_get(idx0,"dataset_description",data_description)
-call ids_get(idx0,"pulse_schedule",pulse_schedule)
-call ids_get(idx0,"pulse_schedule/1",pulse_schedule_term)
-  
+
 write(*,*) 'Finished reading the prescribed IDS'
 call imas_close(idx0)
 
 
+write(*,*) 'Reading the pulse schedule'
+call imas_open_env('ids',pulse_psch,run_psch,idx0,user_psch,database_psch,'3')
+
+call ids_get(idx0,"pulse_schedule",pulse_schedule)
+call ids_get(idx0,"pulse_schedule/1",pulse_schedule_term)
+
+call imas_close(idx0)
 
 !print *,'Press any key to begin simulation...'
 !read (*,*)
--- imas/python_wf/test_python_wf.py
+++ imas/python_wf/test_python_wf.py
@@ -189,10 +189,12 @@ def ASTRA_DENSITY(idslist):
 
 
 class DINA_Workflow:
-  def __init__(self, IMAS_InputStart, IMAS_Output):
+  def __init__(self, IMAS_PulseSchedule, IMAS_InputStart, IMAS_Output):
     
     # imas.DBEntry object containing input data to initialize the workflow
     self.IMAS_InputStart = IMAS_InputStart
+    
+    self.IMAS_PulseSchedule = IMAS_PulseSchedule
     
     # imas.DBEntry object to put output data in
     self.IMAS_Output = IMAS_Output
@@ -284,10 +286,15 @@ class DINA_Workflow:
     
     idslist['wall'] = self.IMAS_InputStart.get('wall')
     idslist['dataset_description'] = self.IMAS_InputStart.get('dataset_description')
-    idslist['pulse_schedule'] = self.IMAS_InputStart.get('pulse_schedule')
-    idslist['pulse_schedule_term'] = self.IMAS_InputStart.get('pulse_schedule', occurrence = 1)
     
     self.IMAS_InputStart.close()
+    
+    
+    self.IMAS_PulseSchedule.open()
+    idslist['pulse_schedule'] = self.IMAS_PulseSchedule.get('pulse_schedule')
+    idslist['pulse_schedule_term'] = self.IMAS_PulseSchedule.get('pulse_schedule', occurrence = 1)
+    self.IMAS_PulseSchedule.close()
+    
     
     #print('Time_Start = ' + str(idslist['equilibrium'].time_slice[0].time), flush=True)
     
@@ -435,6 +442,20 @@ def start(config):
   IMAS_InputStart = imas.DBEntry(imasdef.MDSPLUS_BACKEND, database, pulse, run, username, data_version = '3')
   
   
+  input_psch = root.find('pulse_schedule')
+  usernode = input_psch.find('user')
+  if (usernode != None):
+    username = usernode.text
+  else:
+    username = None
+  if (username == None or username == ""):
+    username = user_default
+  database = input_start.find('database').text
+  pulse = int(input_start.find('pulse').text)
+  run = int(input_start.find('run').text)
+  IMAS_PulseSchedule = imas.DBEntry(imasdef.MDSPLUS_BACKEND, database, pulse, run, username, data_version = '3')
+  
+  
   output = root.find('output')
   database = output.find('database').text
   pulse = int(output.find('pulse').text)
@@ -442,7 +463,7 @@ def start(config):
   IMAS_Output = imas.DBEntry(imasdef.MDSPLUS_BACKEND, database, pulse, run, user_default, data_version = '3')
   
   
-  Workflow = DINA_Workflow(IMAS_InputStart, IMAS_Output)
+  Workflow = DINA_Workflow(IMAS_PulseSchedule, IMAS_InputStart, IMAS_Output)
   Workflow.InterpStart = int(input_start.find('interp_mode').text)
   Workflow.Time_Start = float(input_start.find('time_start').text)
   Workflow.Time_Stop = float(root.find('time_stop').text)
