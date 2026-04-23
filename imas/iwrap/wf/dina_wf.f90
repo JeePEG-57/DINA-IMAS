2561104cc2b58d76dd0f86814f552c4351b9b3b1
= <jean-pierre.svantner@epfl.ch>
2026-04-23 18:26:30 +0200

    dina_wf compatible with AL5 uri=path accepting also backend from wfconfig.xml file

--- imas/iwrap/wf/dina_wf.f90
+++ imas/iwrap/wf/dina_wf.f90
@@ -63,29 +63,24 @@ integer :: ibackend = 12
 ! IDS location data
 character (len=255) :: user_default
 
-character (len=255) :: user_out='', database_out
-integer :: pulse_out=-1, run_out=-1
+character (len=255) :: user_out='', database_out, pulse_out, run_out, back_out
 
-character (len=255) :: user_pfa='', database_pfa
-integer :: pulse_pfa=-1, run_pfa=-1
+character (len=255) :: user_pfa='', database_pfa, pulse_pfa, run_pfa, back_pfa
 
-character (len=255) :: user_pfp='', database_pfp
-integer :: pulse_pfp=-1, run_pfp=-1
+character (len=255) :: user_pfp='', database_pfp, pulse_pfp, run_pfp, back_pfp
 
-character (len=255) :: user_mag='', database_mag
-integer :: pulse_mag=-1, run_mag=-1
+character (len=255) :: user_mag='', database_mag, pulse_mag, run_mag, back_mag
 
-character (len=255) :: user_wll='', database_wll
-integer :: pulse_wll=-1, run_wll=-1
+character (len=255) :: user_wll='', database_wll, pulse_wll, run_wll, back_wll
 
-character (len=255) :: user_prs='', database_prs
-integer :: pulse_prs=-1, run_prs=-1
+character (len=255) :: user_prs='', database_prs, pulse_prs, run_prs, back_prs
 
-character (len=255) :: user_psch='', database_psch
-integer :: pulse_psch=-1, run_psch=-1
+character (len=255) :: user_psch='', database_psch, pulse_psch, run_psch, back_psch
 
-character (len=255) :: user_transp='', database_transp=''
-integer :: pulse_transp=-1, run_transp=-1
+character (len=255) :: user_transp='', database_transp='', pulse_transp='', run_transp='', back_transp=''
+
+
+character (len=511) :: uri
 
 ! Workflow parameters
 real (ids_real) :: pulsetime = 0.0, time_start=0.0, time_stop=10000.0
@@ -140,41 +135,50 @@ call xml2eg_parse_memory(buffer, doc)
   call xml2eg_get(doc, 'pulse_schedule/database', database_psch)
   call xml2eg_get(doc, 'pulse_schedule/pulse', pulse_psch)
   call xml2eg_get(doc, 'pulse_schedule/run', run_psch)
+  call xml2eg_get(doc, 'pulse_schedule/backend', back_psch)
   
   call xml2eg_get(doc, 'input_pf_active/user', user_pfa)
   call xml2eg_get(doc, 'input_pf_active/database', database_pfa)
   call xml2eg_get(doc, 'input_pf_active/pulse', pulse_pfa)
   call xml2eg_get(doc, 'input_pf_active/run', run_pfa)
+  call xml2eg_get(doc, 'input_pf_active/backend', back_pfa)
 
   call xml2eg_get(doc, 'input_pf_passive/user', user_pfp)
   call xml2eg_get(doc, 'input_pf_passive/database', database_pfp)
   call xml2eg_get(doc, 'input_pf_passive/pulse', pulse_pfp)
   call xml2eg_get(doc, 'input_pf_passive/run', run_pfp)
+  call xml2eg_get(doc, 'input_pf_passive/backend', back_pfp)
 
   call xml2eg_get(doc, 'input_magnetics/user', user_mag)
   call xml2eg_get(doc, 'input_magnetics/database', database_mag)
   call xml2eg_get(doc, 'input_magnetics/pulse', pulse_mag)
   call xml2eg_get(doc, 'input_magnetics/run', run_mag)
+  call xml2eg_get(doc, 'input_magnetics/backend', back_mag)
 
   call xml2eg_get(doc, 'input_wall/user', user_wll)
   call xml2eg_get(doc, 'input_wall/database', database_wll)
   call xml2eg_get(doc, 'input_wall/pulse', pulse_wll)
   call xml2eg_get(doc, 'input_wall/run', run_wll)
+  call xml2eg_get(doc, 'input_wall/backend', back_wll)
 
   call xml2eg_get(doc, 'input_start/user', user_prs)
   call xml2eg_get(doc, 'input_start/database', database_prs)
   call xml2eg_get(doc, 'input_start/pulse', pulse_prs)
   call xml2eg_get(doc, 'input_start/run', run_prs)
-  
+  call xml2eg_get(doc, 'input_start/backend', back_prs)
+
   call xml2eg_get(doc, 'input_transp/user', user_transp)
   call xml2eg_get(doc, 'input_transp/database', database_transp)
   call xml2eg_get(doc, 'input_transp/pulse', pulse_transp)
   call xml2eg_get(doc, 'input_transp/run', run_transp)
+  call xml2eg_get(doc, 'input_transp/backend', back_transp)
   
   call xml2eg_get(doc, 'output/user', user_out)
   call xml2eg_get(doc, 'output/database', database_out)
   call xml2eg_get(doc, 'output/pulse', pulse_out)
   call xml2eg_get(doc, 'output/run', run_out)
+  call xml2eg_get(doc, 'output/backend', back_out)
+
   
   
   call xml2eg_get(doc, 'time_start', time_start)
@@ -198,21 +202,21 @@ if (trim(user_transp).eq.'') user_transp = user_default
 if (trim(user_out).eq.'') user_out = user_default
 
 
-print *,' Pulse schedule user, database, pulse, run =', trim(user_psch), trim(database_psch), pulse_psch, run_psch
+print *,' Pulse schedule user, database, pulse, run, backend =', trim(user_psch), trim(database_psch), trim(pulse_psch), trim(run_psch), trim(back_psch)
 
-print *,' PF Active user, database, pulse, run =', trim(user_pfa), trim(database_pfa), pulse_pfa, run_pfa
-print *,' PF Passive user, database, pulse, run =', trim(user_pfp), trim(database_pfp), pulse_pfp, run_pfp
-print *,' Magnetics user, database, pulse, run =', trim(user_mag), trim(database_mag), pulse_mag, run_mag
-print *,' Wall user, database, pulse, run =', trim(user_wll), trim(database_wll), pulse_wll, run_wll
+print *,' PF Active user, database, pulse, run, backend =', trim(user_pfa), trim(database_pfa), trim(pulse_pfa), trim(run_pfa), trim(back_pfa)
+print *,' PF Passive user, database, pulse, run, backend =', trim(user_pfp), trim(database_pfp), trim(pulse_pfp), trim(run_pfp), trim(back_pfp)
+print *,' Magnetics user, database, pulse, run, backend =', trim(user_mag), trim(database_mag), trim(pulse_mag), trim(run_mag), trim(back_mag)
+print *,' Wall user, database, pulse, run, backend =', trim(user_wll), trim(database_wll), trim(pulse_wll), trim(run_wll), trim(back_wll)
 
-print *,' Start user, database, pulse, run =', trim(user_prs), trim(database_prs), pulse_prs, run_prs
+print *,' Start user, database, pulse, run, backend =', trim(user_prs), trim(database_prs), trim(pulse_prs), trim(run_prs), trim(back_prs)
 print *,' Start time, s =', time_start
 print *,' Start interpolation =', interp_start
 
-print *,' Transp user, database, pulse, run =', trim(user_transp), trim(database_transp), pulse_transp, run_transp
+print *,' Transp user, database, pulse, run, backend =', trim(user_transp), trim(database_transp), trim(pulse_transp), trim(run_transp), trim(back_transp)
 print *,' Transp interpolation =', interp_transp
 
-print *,' Output user =', trim(user_out), trim(database_out), pulse_out, run_out
+print *,' Output user =', trim(user_out), trim(database_out), trim(pulse_out), trim(run_out), trim(back_out)
 print *,' Output put decimation =', idec
 
 print *,' External transport time, s =', time_ext
@@ -227,7 +231,13 @@ endif
 
 
 write(*,*) 'Reading the prescribed IDS'
- call imas_open_env('ids',pulse_prs,run_prs,idx0,user_prs,database_prs,'3')
+uri = 'imas:'//trim(back_prs)//'?path=/home/ITER/'//trim(user_prs)//'/public/imasdb/'//trim(database_prs)//'/3/'//trim(pulse_prs)//'/'//trim(run_prs) 
+write(*,*) 'URI for restart input IDS =', trim(uri)
+call imas_open(trim(uri),40,idx0,error_flag)
+if (error_flag.ne.0) then
+  write(*,*) 'Error opening the restart IDS, error_flag =', error_flag
+  stop
+endif
 
 ! call ids_get(idx0, "workflow", workflow)
 
@@ -272,20 +282,27 @@ endif
 write(*,*) 'Finished reading the prescribed IDS'
 call imas_close(idx0)
 
-
-call imas_open_env('ids',pulse_pfa,run_pfa,idx_a,user_pfa,database_pfa,'3')
+uri = 'imas:'//trim(back_pfa)//'?path=/home/ITER/'//trim(user_pfa)//'/public/imasdb/'//trim(database_pfa)//'/3/'//trim(pulse_pfa)//'/'//trim(run_pfa) 
+write(*,*) 'URI for pf_active input IDS =', trim(uri)
+call imas_open(trim(uri),40,idx_a,error_flag)
 call ids_get_slice(idx_a,"pf_active",pf_active0, time_start, interp_start)
 call imas_close(idx_a)
 
-call imas_open_env('ids',pulse_pfp,run_pfp,idx_p,user_pfp,database_pfp,'3')
+uri = 'imas:'//trim(back_pfp)//'?path=/home/ITER/'//trim(user_pfp)//'/public/imasdb/'//trim(database_pfp)//'/3/'//trim(pulse_pfp)//'/'//trim(run_pfp) 
+write(*,*) 'URI for pf_passive input IDS =', trim(uri)
+call imas_open(trim(uri),40,idx_p,error_flag)
 call ids_get_slice(idx_p,"pf_passive",pf_passive0, time_start, interp_start)
 call imas_close(idx_p)
 
-call imas_open_env('ids',pulse_mag,run_mag,idx_m,user_mag,database_mag,'3')
+uri = 'imas:'//trim(back_mag)//'?path=/home/ITER/'//trim(user_mag)//'/public/imasdb/'//trim(database_mag)//'/3/'//trim(pulse_mag)//'/'//trim(run_mag) 
+write(*,*) 'URI for magnetics input IDS =', trim(uri)
+call imas_open(trim(uri),40,idx_m,error_flag)
 call ids_get_slice(idx_m,"magnetics",magnetics0, time_start, interp_start)
 call imas_close(idx_m)
 
-call imas_open_env('ids',pulse_wll,run_wll,idx0,user_wll,database_wll,'3')
+uri = 'imas:'//trim(back_wll)//'?path=/home/ITER/'//trim(user_wll)//'/public/imasdb/'//trim(database_wll)//'/3/'//trim(pulse_wll)//'/'//trim(run_wll) 
+write(*,*) 'URI for wall input IDS =', trim(uri)
+call imas_open(trim(uri),40,idx0,error_flag)
 call ids_get_slice(idx0,"wall",wall, time_start, interp_start)
 call imas_close(idx0)
 
@@ -321,7 +338,9 @@ endif
 
 
 write(*,*) 'Reading the pulse schedule'
-call imas_open_env('ids',pulse_psch,run_psch,idx0,user_psch,database_psch,'3')
+uri = 'imas:'//trim(back_psch)//'?path=/home/ITER/'//trim(user_psch)//'/public/imasdb/'//trim(database_psch)//'/3/'//trim(pulse_psch)//'/'//trim(run_psch) 
+write(*,*) 'URI for pf_passive input IDS =', trim(uri)
+call imas_open(trim(uri),40,idx0,error_flag)
 
 call ids_get(idx0,"pulse_schedule",pulse_schedule)
 call ids_get(idx0,"pulse_schedule/1",pulse_schedule_term)
@@ -336,8 +355,13 @@ call imas_close(idx0)
  
   
   
-
-  call imas_create_env('ids',pulse_out,run_out,1,1,idx,user_out,database_out,'3')
+  uri = 'imas:'//trim(back_out)//'?path=/home/ITER/'//trim(user_out)//'/public/imasdb/'//trim(database_out)//'/3/'//trim(pulse_out)//'/'//trim(run_out) 
+  write(*,*) 'URI for output IDS =', trim(uri)
+  call imas_open(trim(uri),43,idx,error_flag)
+  if (error_flag.ne.0) then
+    write(*,*) 'Error opening the output IDS, error_flag =', error_flag
+    stop
+  endif
   write(*,*) 'Pulse file is created'
 
   call ids_put(idx,"wall",wall)
@@ -409,6 +433,9 @@ call ids_deallocate(core_profiles0)
 call ids_deallocate(core_sources0)
 write(*,*) "DINA_IMAS inputs deallocated"
 flush(6)
+
+write(*,*) '!!!!!!!!!!!!!!!!   IT WAS JUST TO TEST    !!!!!!!!!!!!!!!'
+exit 
 
 
 call kmc_step(pulse_schedule, pulse_schedule_term, equilibrium, pf_active1, pf_active &
@@ -509,7 +536,9 @@ write(*,*) 'Using prescribed transport'
   !ibackend = 13 ! HDF5
 
   !call system("export IMAS_AL_DEFAULT_BACKEND=13")
-  call imas_open_env('ids',pulse_transp,run_transp,idx0,user_transp,database_transp,'3')
+uri = 'imas:'//trim(back_transp)//'?path=/home/ITER/'//trim(user_transp)//'/public/imasdb/'//trim(database_transp)//'/3/'//trim(pulse_transp)//'/'//trim(run_transp) 
+write(*,*) 'URI for external transport input IDS =', trim(uri)
+call imas_open(trim(uri),40,idx0,error_flag)
   
   call ids_get_slice(idx0,"core_profiles",core_profiles0, time_get, interp_transp)
   call ids_get_slice(idx0,"core_sources",core_sources0, time_get, interp_transp)
