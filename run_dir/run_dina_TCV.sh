ea0c563fea4ebb7d0087a368af2f4c93e259518d
= <jean-pierre.svantner@epfl.ch>
2026-04-27 14:50:30 +0200

    moved run_dir directory to root

--- run_dir/run_dina_TCV.sh
+++ run_dir/run_dina_TCV.sh
@@ -2,4 +2,4 @@
 python copy_input_parameters.py wfconfig.xml --source-dir . --base-path /home/ITER
 
 # run dina, and output logs to log_fortran in cwd. 
-../iwrap/wf_tcv/dina_wf.exe wfconfig.xml 2>&1 | tee log_fortran
+../imas/iwrap/wf_tcv/dina_wf.exe wfconfig.xml 2>&1 | tee log_fortran
