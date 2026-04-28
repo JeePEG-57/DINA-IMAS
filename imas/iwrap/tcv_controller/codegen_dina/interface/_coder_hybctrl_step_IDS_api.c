8783b68d4bc58daa039c8a29eda22201273b4ce6
= <jean-pierre.svantner@epfl.ch>
2026-04-28 10:38:26 +0200

    Fix the segfault in IDS2Ain.c in tcv_controller_step

--- imas/iwrap/tcv_controller/codegen_dina/interface/_coder_hybctrl_step_IDS_api.c
+++ imas/iwrap/tcv_controller/codegen_dina/interface/_coder_hybctrl_step_IDS_api.c
@@ -5,7 +5,7 @@
  * File: _coder_hybctrl_step_IDS_api.c
  *
  * MATLAB Coder version            : 5.4
- * C/C++ source code generated on  : 24-Apr-2026 10:42:42
+ * C/C++ source code generated on  : 28-Apr-2026 10:26:51
  */
 
 /* Include Files */
--- imas/iwrap/tcv_controller/codegen_dina/str2double.h
+++ imas/iwrap/tcv_controller/codegen_dina/str2double.h
@@ -5,7 +5,7 @@
  * File: str2double.h
  *
  * MATLAB Coder version            : 5.4
- * C/C++ source code generated on  : 24-Apr-2026 10:42:42
+ * C/C++ source code generated on  : 28-Apr-2026 10:26:51
  */
 
 #ifndef STR2DOUBLE_H
