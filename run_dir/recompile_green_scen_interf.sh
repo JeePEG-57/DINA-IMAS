3ee39358bd0129945307c1d82a8f9e76af01bf8e
= <jean-pierre.svantner@epfl.ch>
2026-04-29 15:59:10 +0200

    Untrack run_dir

--- run_dir/recompile_green_scen_interf.sh
+++
@@ -1,8 +0,0 @@
-cd /home/ITER/svantnj/code/dina/dinatcv/src/green
-make
-cd /home/ITER/svantnj/code/dina/dinatcv/src/scenario
-make
-cd /home/ITER/svantnj/code/dina/dinatcv/imas/interface
-make
-cd /home/ITER/svantnj/code/dina/dinatcv/imas/python_wf
-
