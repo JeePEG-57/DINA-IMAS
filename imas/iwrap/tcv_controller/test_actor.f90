8555880cc2eacbb2f00b92eaa4668a20732e8d08
= <jean-pierre.svantner@epfl.ch>
2026-05-07 16:20:50 +0200

    Updated test tcv_controller First copies the binary files from input uri, then runs the tests

--- imas/iwrap/tcv_controller/test_actor.f90
+++ imas/iwrap/tcv_controller/test_actor.f90
@@ -79,11 +79,15 @@ program TCV_controller_test
 
 
 
+    ! ----------------- COPY BIN FILES ---------------
+    call copy_bin_files(uri_in)
+
 
     ! ----------------- PREPARE OUTPUT DB ---------------
     call imas_open(uri_out, CREATE_PULSE, idx_out, error_flag)
     print *, 'Output database is created'
     
+
     ! ----------------- OPEN INPUT DB ---------------
     ! Open DB
     call imas_open(uri_in, OPEN_PULSE, idx_in, error_flag)
@@ -93,6 +97,7 @@ program TCV_controller_test
         print *, 'Error creating input database: ', error_message
         stop
     end if
+
 
 
     ! ----------------- DO SIMULATION ---------------
@@ -114,7 +119,7 @@ program TCV_controller_test
         print *,  'Put magnetics/1'
         call ids_put_slice(idx_out,"magnetics/1",magnetics0)
         ! Update time 
-        time_get = time_get + time_increment ! advance by 1 ms
+        time_get = time_get + time_increment ! advance 
         ! Deallocate previous IDSs
         call ids_deallocate(magnetics0)
         call ids_deallocate(pf_active0)
@@ -129,4 +134,79 @@ program TCV_controller_test
         
     print *, 'IDS deallocated'
     print *, 'Test TCV controller file exiting cleanly'
+
+
+
+    contains
+    subroutine copy_bin_files(uri_read)
+        character(len=255) :: src, dst
+        character(len=255), dimension(:), allocatable :: filePathsBin
+        integer :: i, nFiles=3
+        integer :: pos, pos2, start
+        character(len=255) :: pathval
+        character(len=255), intent(in) :: uri_read
+
+        ! Extract the path component after "path=" in uri_read
+        print *, 'Extracting path from uri_read: ', uri_read
+        pos = index(uri_read, 'path=')
+        if (pos > 0) then
+            start = pos + 5  ! right after "path="
+            pos2 = index(uri_read(start:), '&') ! & --> end of string
+            if (pos2 > 0) then
+                pathval = adjustl(uri_read(start:start+pos2-2))
+            else
+                pathval = adjustl(trim(uri_read(start:)))
+            end if
+            ! strip surrounding quotes if present
+            if (len_trim(pathval) >= 2) then
+                if ((pathval(1:1) == '"' .and. pathval(len_trim(pathval):len_trim(pathval)) == '"') .or. &
+                    (pathval(1:1) == "'" .and. pathval(len_trim(pathval):len_trim(pathval)) == "'")) then
+                    pathval = pathval(2:len_trim(pathval)-1)
+                else
+                    pathval = pathval(:len_trim(pathval))
+                end if
+            else
+                pathval = pathval(:len_trim(pathval))
+            end if
+        else
+            pathval = ''
+        end if
+
+        ! Use extracted path as source directory
+        if (len_trim(pathval) > 0) then
+            src = trim(pathval)
+            print *, 'Extracted path from uri_read:', src
+        else
+            print *, 'No path= found in uri_read; src left empty'
+            src = ''
+        end if
+
+        ! Define the list of BIN files to copy (this should be set according to your needs)
+        allocate(filePathsBin(nFiles))
+        filePathsBin(1) = trim(src)//'/cpar.bin'
+        filePathsBin(2) = trim(src)//'/Arefs.bin'
+        filePathsBin(3) = trim(src)//'/ffs.bin'
+
+        do i = 1, nFiles
+            ! Check file exists; if not, skip this iteration
+            open(unit=100, file=trim(filePathsBin(i)), status='old', action='read', iostat=pos2)
+            if (pos2 /= 0) then
+                print *,'File does not exist, skipping: ', trim(filePathsBin(i))
+                pos2 = 0
+                cycle
+            else
+                close(100, iostat=pos2)
+                pos2 = 0
+            end if
+            src = trim(filePathsBin(i))
+            dst = './'
+            ! Copy file using system 'cp' command and check exit status (POSIX)
+            call execute_command_line('cp "'//trim(src)//'" "'//trim(dst)//'"', wait=.true., exitstat=pos2)
+            if (pos2 == 0) then
+                print *, 'Copied BIN file:', src
+            else
+                print *, 'Failed to copy BIN file:', src, ' exit status=', pos2
+            end if
+        end do
+    end subroutine copy_bin_files
 end program TCV_controller_test
