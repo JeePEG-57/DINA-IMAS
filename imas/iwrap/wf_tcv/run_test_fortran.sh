a556bef6772c90738027e4681ab8f25f066204d7
= <jean-pierre.svantner@epfl.ch>
2026-05-08 14:25:10 +0200

    Removed also machines/tcv directory

--- imas/iwrap/wf_tcv/run_test_fortran.sh
+++
@@ -1,9 +0,0 @@
-mkdir sandbox
-cd sandbox
-rm -rf ./*
-
-cp -rf $DINA_ROOT/imas/iwrap/dina_green/code_parameters.xml ./codeparam_green.xml
-cp -rf $DINA_ROOT/machines/tcv/codeparam_dina.xml ./codeparam_dina.xml
-cp -rf $DINA_ROOT/machines/imp ./
-
-$DINA_ROOT/imas/iwrap/wf_tcv/dina_wf.exe ../wfconfig.xml 2>&1 | tee log_fortran
